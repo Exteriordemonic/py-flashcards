f8066d44d6347d96528a43b10ae9c360ca75c256
Kamil <kmirosz6@gmail.com>
2026-04-08 12:28:08 +0200

    chore: Remove unused test file for flashcards

---
+++ flashcards/tests/test_urls.py
@@ -0,0 +1,132 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from flashcards.models import Deck, Flashcard
+
+
+User = get_user_model()
+
+
+class TestUrls(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testuser", password="testpassword"
+        )
+        self.client.login(username="testuser", password="testpassword")
+
+    def create_flashcard(self):
+        return Flashcard.objects.create(
+            question="Jaka jest stolica Polski?",
+            answer_a="Gdańsk",
+            answer_b="Kraków",
+            answer_c="Warszawa",
+            answer_d="Wrocław",
+            correct_answer="Warszawa",
+            created_by=self.user,
+        )
+
+    def create_deck(self):
+        return Deck.objects.create(name="Example Deck 1", owner=self.user)
+
+    def test_flashcard_list_is_resolved(self):
+        response = self.client.get(reverse("flashcards:flashcard-list"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_flashcard_detail_is_resolved(self):
+        flashcard = self.create_flashcard()
+
+        response = self.client.get(
+            reverse("flashcards:flashcard-detail", kwargs={"pk": flashcard.id})
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_flashcard_create_is_resolved(self):
+        response = self.client.get(reverse("flashcards:flashcard-create"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_flashcard_update_is_resolved(self):
+        flashcard = self.create_flashcard()
+
+        response = self.client.get(
+            reverse("flashcards:flashcard-update", kwargs={"pk": flashcard.id})
+        )
+        self.assertEqual(
+            response.status_code,
+            200,
+        )
+
+    def test_flashcard_delete_is_resolved(self):
+        flashcard = self.create_flashcard()
+
+        response = self.client.get(
+            reverse("flashcards:flashcard-delete", kwargs={"pk": flashcard.id})
+        )
+        self.assertEqual(
+            response.status_code,
+            200,
+        )
+
+    def test_deck_list_is_resolved(self):
+        response = self.client.get(reverse("flashcards:deck-list"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_deck_detail_is_resolved(self):
+        deck = self.create_deck()
+
+        response = self.client.get(
+            reverse("flashcards:deck-detail", kwargs={"pk": deck.id})
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_deck_create_is_resolved(self):
+        response = self.client.get(reverse("flashcards:deck-create"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_deck_update_is_resolved(self):
+        deck = self.create_deck()
+
+        response = self.client.get(
+            reverse("flashcards:deck-update", kwargs={"pk": deck.id})
+        )
+        self.assertEqual(
+            response.status_code,
+            200,
+        )
+
+    def test_deck_delete_is_resolved(self):
+        deck = self.create_deck()
+
+        response = self.client.get(
+            reverse("flashcards:deck-delete", kwargs={"pk": deck.id})
+        )
+        self.assertEqual(
+            response.status_code,
+            200,
+        )
+
+    def test_not_logged_in_access(self):
+        self.client.logout()
+        flashcard = self.create_flashcard()
+        deck = self.create_deck()
+        urls = [
+            reverse("flashcards:flashcard-list"),
+            reverse("flashcards:flashcard-create"),
+            reverse(
+                "flashcards:flashcard-detail", kwargs={"pk": flashcard.id}
+            ),
+            reverse(
+                "flashcards:flashcard-update", kwargs={"pk": flashcard.id}
+            ),
+            reverse(
+                "flashcards:flashcard-delete", kwargs={"pk": flashcard.id}
+            ),
+            reverse("flashcards:deck-list"),
+            reverse("flashcards:deck-create"),
+            reverse("flashcards:deck-detail", kwargs={"pk": deck.id}),
+            reverse("flashcards:deck-update", kwargs={"pk": deck.id}),
+            reverse("flashcards:deck-delete", kwargs={"pk": deck.id}),
+        ]
+
+        for url in urls:
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 302)
