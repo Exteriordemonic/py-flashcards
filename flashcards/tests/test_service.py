1a867387499e40c03dffd79c84d92e11ec0fc010
Kamil <kmirosz6@gmail.com>
2026-04-11 16:20:43 +0200

    refactor: Convert FlashcardService tests from TestCase to pytest fixtures and functions for improved readability and maintainability

--- flashcards/tests/test_service.py
+++ flashcards/tests/test_service.py
@@ -1,125 +1,158 @@
+from collections import Counter
+
+import pytest
 from django.contrib.auth import get_user_model
-from django.test import TestCase
 
 from decks.models import Deck
 from flashcards.models import Flashcard
-from flashcards.services import FlashcardService, AnswerInput
+from flashcards.services import AnswerInput, FlashcardService
 
 User = get_user_model()
 
 
-class FlashcardServiceTest(TestCase):
-    def setUp(self) -> None:
-        self.user = User.objects.create_user(
-            username="user1", password="password1"
-        )
-        self.deck = Deck.objects.create(name="Deck Name", owner=self.user)
+@pytest.fixture
+def user(db):
+    return User.objects.create_user(
+        username="user1",
+        password="password1",
+    )
 
-        self.data = {
-            "question": "Capitol of Poland?",
-            "created_by": self.user,
-            "deck": self.deck,
-            "answers": [
-                AnswerInput(text="Warsaw", is_correct=True),
-                AnswerInput(text="Paris"),
-                AnswerInput(text="Berlin"),
-            ],
-        }
 
-    def test_correct_data(self):
-        flashcard = FlashcardService.create_flashcard(**self.data)
+@pytest.fixture
+def deck(user):
+    return Deck.objects.create(name="Deck Name", owner=user)
 
-        self.assertEqual(flashcard.question, self.data["question"])
-        self.assertEqual(flashcard.answers.count(), len(self.data["answers"]))
-        self.assertEqual(flashcard.deck, self.data["deck"])
-        self.assertEqual(flashcard.created_by, self.data["created_by"])
 
-        correct_answers = flashcard.answers.filter(is_correct=True)
-        self.assertEqual(correct_answers.count(), 1)
+@pytest.fixture
+def flashcard_create_data(user, deck):
+    return {
+        "question": "Capitol of Poland?",
+        "created_by": user,
+        "deck": deck,
+        "answers": make_answers(),
+    }
 
-        self.assertTrue(
-            flashcard.answers.filter(text="Warsaw", is_correct=True).exists()
-        )
 
-        texts = list(flashcard.answers.values_list("text", flat=True))
-        self.assertCountEqual(texts, ["Warsaw", "Paris", "Berlin"])
+def make_answers():
+    return [
+        AnswerInput(text="Warsaw", is_correct=True),
+        AnswerInput(text="Paris"),
+        AnswerInput(text="Berlin"),
+    ]
 
-    def test_deck_from_string(self):
-        data = {**self.data, "deck": "deck"}
 
-        flashcard = FlashcardService.create_flashcard(**data)
-        self.assertEqual(flashcard.deck.name, "deck")
-        self.assertEqual(Deck.objects.count(), 2)
+def test_creating_flashcard(user, deck):
+    flashcard = FlashcardService.create_flashcard(
+        question="Capitol of Poland?",
+        created_by=user,
+        deck=deck,
+        answers=[
+            AnswerInput(text="Warsaw", is_correct=True),
+            AnswerInput(text="Paris"),
+            AnswerInput(text="Berlin"),
+        ],
+    )
 
-        self.assertTrue(
-            Deck.objects.filter(name="deck", owner=self.user).exists()
-        )
+    assert flashcard.question == "Capitol of Poland?"
+    assert flashcard.created_by == user
+    assert flashcard.deck == deck
+    assert flashcard.answers.count() == 3
+    assert flashcard.answers.filter(text="Warsaw", is_correct=True).exists()
 
-    def test_deck_from_string_that_exists(self):
-        data = {**self.data, "deck": "Deck Name"}
 
-        flashcard = FlashcardService.create_flashcard(**data)
+def test_correct_data(flashcard_create_data):
+    flashcard = FlashcardService.create_flashcard(**flashcard_create_data)
 
-        self.assertEqual(flashcard.deck, self.deck)
-        self.assertEqual(Deck.objects.count(), 1)
+    assert flashcard.question == flashcard_create_data["question"]
+    assert flashcard.answers.count() == len(flashcard_create_data["answers"])
+    assert flashcard.deck == flashcard_create_data["deck"]
+    assert flashcard.created_by == flashcard_create_data["created_by"]
 
-    def test_data_without_answers(self):
-        data = {**self.data, "answers": []}
-        with self.assertRaises(ValueError):
-            FlashcardService.create_flashcard(**data)
+    correct_answers = flashcard.answers.filter(is_correct=True)
+    assert correct_answers.count() == 1
 
-    def test_data_without_correct_answers(self):
-        data = {
-            **self.data,
-            "answers": [
-                AnswerInput(text=a.text, is_correct=False)
-                for a in self.data["answers"]
-            ],
-        }
+    assert flashcard.answers.filter(text="Warsaw", is_correct=True).exists()
 
-        with self.assertRaises(ValueError):
-            FlashcardService.create_flashcard(**data)
+    texts = list(flashcard.answers.values_list("text", flat=True))
+    assert Counter(texts) == Counter(["Warsaw", "Paris", "Berlin"])
 
-    def test_all_correct_answers(self):
-        data = {
-            **self.data,
-            "answers": [
-                AnswerInput(text=a.text, is_correct=True)
-                for a in self.data["answers"]
-            ],
-        }
 
-        flashcard = FlashcardService.create_flashcard(**data)
-        self.assertEqual(
-            flashcard.answers.filter(is_correct=True).count(),
-            len(data["answers"]),
-        )
+def test_deck_from_string(flashcard_create_data, user):
+    data = {**flashcard_create_data, "deck": "deck"}
 
-    def test_multiple_correct_answers(self):
-        orig = self.data["answers"]
-        data = {
-            **self.data,
-            "answers": [
-                orig[0],
-                AnswerInput(text=orig[1].text, is_correct=True),
-                orig[2],
-            ],
-        }
+    flashcard = FlashcardService.create_flashcard(**data)
+    assert flashcard.deck.name == "deck"
+    assert Deck.objects.count() == 2
 
-        correct_answers = len(
-            [answer for answer in data["answers"] if answer.is_correct]
-        )
+    assert Deck.objects.filter(name="deck", owner=user).exists()
 
-        flashcard = FlashcardService.create_flashcard(**data)
-        self.assertEqual(
-            flashcard.answers.filter(is_correct=True).count(),
-            correct_answers,
-        )
 
-    def test_no_flashcard_created_when_answers_invalid(self):
-        data = {**self.data, "answers": None}
+def test_deck_from_string_that_exists(flashcard_create_data, deck):
+    data = {**flashcard_create_data, "deck": "Deck Name"}
 
-        with self.assertRaises(Exception):
-            FlashcardService.create_flashcard(**data)
+    flashcard = FlashcardService.create_flashcard(**data)
 
-        self.assertEqual(Flashcard.objects.count(), 0)
+    assert flashcard.deck == deck
+    assert Deck.objects.count() == 1
+
+
+def test_data_without_answers(flashcard_create_data):
+    data = {**flashcard_create_data, "answers": []}
+    with pytest.raises(ValueError):
+        FlashcardService.create_flashcard(**data)
+
+
+def test_data_without_correct_answers(flashcard_create_data):
+    data = {
+        **flashcard_create_data,
+        "answers": [
+            AnswerInput(text=a.text, is_correct=False)
+            for a in flashcard_create_data["answers"]
+        ],
+    }
+
+    with pytest.raises(ValueError):
+        FlashcardService.create_flashcard(**data)
+
+
+def test_all_correct_answers(flashcard_create_data):
+    data = {
+        **flashcard_create_data,
+        "answers": [
+            AnswerInput(text=a.text, is_correct=True)
+            for a in flashcard_create_data["answers"]
+        ],
+    }
+
+    flashcard = FlashcardService.create_flashcard(**data)
+    assert flashcard.answers.filter(is_correct=True).count() == len(
+        data["answers"]
+    )
+
+
+def test_multiple_correct_answers(flashcard_create_data):
+    orig = flashcard_create_data["answers"]
+    data = {
+        **flashcard_create_data,
+        "answers": [
+            orig[0],
+            AnswerInput(text=orig[1].text, is_correct=True),
+            orig[2],
+        ],
+    }
+
+    correct_answers = len(
+        [answer for answer in data["answers"] if answer.is_correct]
+    )
+
+    flashcard = FlashcardService.create_flashcard(**data)
+    assert flashcard.answers.filter(is_correct=True).count() == correct_answers
+
+
+def test_no_flashcard_created_when_answers_invalid(flashcard_create_data):
+    data = {**flashcard_create_data, "answers": None}
+
+    with pytest.raises(Exception):
+        FlashcardService.create_flashcard(**data)
+
+    assert Flashcard.objects.count() == 0
